bb50c02ddf31922040c4e693975011e78b5252c9
Ivan Assenov <ivanassenov@IASSENOV-MBP.attlocal.net>
2024-11-23 17:03:19 -0500

    added results

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import FileUpload from './components/FileUpload';
+import Results from './pages/results'; // Make sure the path is correct
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<FileUpload />} />
+        <Route path="/results" element={<Results />} /> {/* Define the /results route */}
       </Routes>
     </Router>
   );
